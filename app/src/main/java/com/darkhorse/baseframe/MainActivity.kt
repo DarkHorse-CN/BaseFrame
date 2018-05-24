89d5d844b7648b5b419722daf141ebd51fdf980f
DarkHorse_czj <523479985@qq.com>
2018-05-24 12:41:20 +0800

    添加统一依赖管理

--- app/src/main/java/com/darkhorse/baseframe/MainActivity.kt
+++ app/src/main/java/com/darkhorse/baseframe/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.darkhorse.baseframe
 
 import android.content.Intent
-import android.view.View
 import android.provider.MediaStore
+import android.view.View
 import com.blankj.ALog
-import com.darkhorse.baseframe.R.id.btn_access_camera
 import com.darkhorse.baseframe.permission.PermissionActivity
 import com.darkhorse.baseframe.permission.PermissionCode
+import kotlinx.android.synthetic.main.activity_main.*
 import pub.devrel.easypermissions.AfterPermissionGranted
 
 
-class MainActivity : PermissionActivity() {
+class MainActivity : PermissionActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v){
+            btn_access_camera -> requestPermission(PermissionCode.CAMERA)
+        }
+    }
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
@@ -23,6 +28,7 @@ class MainActivity : PermissionActivity() {
     }
 
     override fun initListener() {
+        btn_access_camera.setOnClickListener(this)
     }
 
     override fun initData() {
